dc4ac54625cb4adb22c29f06ad04b38e7676c0e1
Venkatesh Katkuri <142483813+venkateshkatkuri512@users.noreply.github.com>
2023-11-13 11:12:25 +0530

    Load

---
+++ script.js
@@ -0,0 +1,27 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", function () {
+  let input = document.getElementById("result");
+  let buttons = document.querySelectorAll("button");
+  let string = "";
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonText = button.innerHTML;
+      switch (buttonText) {
+        case "AC":
+          string = "";
+          break;
+        case "DEL":
+          string = string.slice(0, -1);
+          break;
+        case "=":
+          string = eval(string);
+          break;
+        default:
+          string += buttonText;
+          break;
+      }
+      input.value = string;
+    });
+  });
+});
